d11841ab68dfaea78c57f6223a421a503454eaa5
Asher Dill <aldill323@access.alamancecc.edu>
2026-09-05 19:45:05 -0700

    update lab1 clock

--- src/module_0/lab1_clock.py
+++ src/module_0/lab1_clock.py
@@ -1,9 +1,10 @@
 # Refer to this module's readme
-time = 1741604700
-timezone = "US/Eastern"
 
-print(time, "in", timezone)
+def main():
+    time = 1741604700
+    timezone = "US/Eastern"
+    print(time, "in", timezone)
+    time = time + 60
+    print(time)
 
-time = time + 60
-
-print(time)
+main()
